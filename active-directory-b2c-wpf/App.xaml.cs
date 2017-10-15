f3de7c56f3fa5d2e871f3865ffe42fc90e67e2d4
Sara Ford <saraford@users.noreply.github.com>
2017-10-15 13:10:18 -0700

    updated settings for my tenant & added external app.config file
    added an external config file to prevent sensitive settings values from being added to git

--- active-directory-b2c-wpf/App.xaml.cs
+++ active-directory-b2c-wpf/App.xaml.cs
@@ -14,14 +14,17 @@ namespace active_directory_b2c_wpf
     /// </summary>
     public partial class App : Application
     {
-        private static string Tenant = "fabrikamb2c.onmicrosoft.com";
-        private static string ClientId = "841e1190-d73a-450c-9d68-f5cf16b78e81";
-        public static string PolicySignUpSignIn = "b2c_1_susi";
+        private static string Tenant = "dorfarasB2CTenant.onmicrosoft.com";
+        private static string ClientId = ConfigurationManager.AppSettings["ClientId"];
+        public static string PolicySignUpSignIn = "b2c_1_SiUpIn";
         public static string PolicyEditProfile = "b2c_1_edit_profile";
         public static string PolicyResetPassword = "b2c_1_reset";
 
-        public static string[] ApiScopes = { "https://fabrikamb2c.onmicrosoft.com/demoapi/demo.read" };
-        public static string ApiEndpoint = "https://fabrikamb2chello.azurewebsites.net/hello";
+        //public static string[] ApiScopes = { "https://fabrikamb2c.onmicrosoft.com/demoapi/demo.read" };
+        //public static string ApiEndpoint = "https://fabrikamb2chello.azurewebsites.net/hello";
+
+        public static string[] ApiScopes = { "https://dorfarasB2CTenant.onmicrosoft.com/helloapi/hello" };
+        public static string ApiEndpoint = "http://localhost:55259/hello";
 
         private static string BaseAuthority = "https://login.microsoftonline.com/tfp/{tenant}/{policy}/oauth2/v2.0/authorize";
         public static string Authority = BaseAuthority.Replace("{tenant}", Tenant).Replace("{policy}", PolicySignUpSignIn);
