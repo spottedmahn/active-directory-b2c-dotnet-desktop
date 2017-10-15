1580d84fa77229f6edeeb5500c89af6a97a94d6f
Sara Ford <saraford@users.noreply.github.com>
2017-10-15 12:15:09 -0700

    added authentication to API

---
+++ HelloAPI/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Owin;
+using System;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin;
+using HelloAPI.App_Start;
+using System.Configuration;
+
+namespace HelloAPI
+{
+    public partial class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
